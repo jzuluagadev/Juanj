0686eb3cbcfed784964c4a1a82a059384eb48e29
Juan <jzuluagagomezbotero@gmail.com>
2025-04-02 20:41:57 -0500

    Create new cards

--- init_db.py
+++ init_db.py
@@ -26,27 +26,59 @@ def init_db():
         # Datos de productos
         products = [
             {
-                'name': 'Pulsera Miyuki',
-                'description': 'Pulsera elegante con cuentas Miyuki en tonos dorados',
-                'price': 89.99,
-                'stock': 10,
-                'material': 'Cuentas Miyuki',
-                'color': 'Dorado',
-                'dimensions': '18cm',
+                'name': 'Pulsera M',
+                'description': 'Pulsera M',
+                'price': 29.99,
+                'stock': 12,
+                'material': 'Metal y cristales',
+                'color': 'Plateado',
+                'dimensions': 'Ajustable, aproximadamente 18cm',
                 'images': [
-                    {'url': 'img/products/joyas1.png', 'is_main': True},
-                    {'url': 'img/products/joyas2.png', 'is_main': False},
-                    {'url': 'img/products/joyas3.png', 'is_main': False}
+                    {'url': 'img/products/pulseraM1.jpg', 'is_main': True},
+                    {'url': 'img/products/pulseraM2.jpg', 'is_main': False},
+                    {'url': 'img/products/pulseraM3.jpg', 'is_main': False},
+                    {'url': 'img/products/pulseraM4.jpg', 'is_main': False}
                 ]
             },
             {
-                'name': 'Collar Cristales',
-                'description': 'Elegante gargantilla con cristales facetados disponible en tres colores: plateado, negro y blanco',
-                'price': 79.99,
+                'name': 'Aretes Maxi',
+                'description': 'Maxiaretes',
+                'price': 34.99,
                 'stock': 15,
-                'material': 'Cristales facetados',
-                'color': 'Plateado/Negro/Blanco',
-                'dimensions': '35cm',
+                'material': 'Metal bañado en oro',
+                'color': 'Dorado',
+                'dimensions': 'Largo: 8cm',
+                'images': [
+                    {'url': 'img/products/maxi1.jpg', 'is_main': True},
+                    {'url': 'img/products/maxi2.jpg', 'is_main': False},
+                    {'url': 'img/products/maxi3.jpg', 'is_main': False},
+                    {'url': 'img/products/maxi4.jpg', 'is_main': False},
+                    {'url': 'img/products/maxi5.jpg', 'is_main': False},
+                    {'url': 'img/products/maxi6.jpg', 'is_main': False}
+                ]
+            },
+            {
+                'name': 'Pulsera Miyuki',
+                'description': 'Hermosa pulsera hecha con cuentas Miyuki de alta calidad. Perfecta para cualquier ocasión.',
+                'price': 29.99,
+                'stock': 10,
+                'material': 'Cuentas Miyuki, hilo elástico',
+                'color': 'Dorado',
+                'dimensions': 'Ajustable, aproximadamente 18cm',
+                'images': [
+                    {'url': 'img/products/pulsoh1.jpg', 'is_main': True},
+                    {'url': 'img/products/pulsoh2.jpg', 'is_main': False},
+                    {'url': 'img/products/pulsoh3.jpg', 'is_main': False}
+                ]
+            },
+            {
+                'name': 'Choker Elegante',
+                'description': 'Choker elegante con diseño minimalista. Ideal para eventos especiales.',
+                'price': 24.99,
+                'stock': 15,
+                'material': 'Tela satinada, broche ajustable',
+                'color': 'Negro',
+                'dimensions': 'Ajustable, 35-40cm',
                 'images': [
                     {'url': 'img/products/choker1.png.jpg', 'is_main': True},
                     {'url': 'img/products/choker2.png.jpg', 'is_main': False},
@@ -55,33 +87,88 @@ def init_db():
                 ]
             },
             {
-                'name': 'Pulsera Elegante',
-                'description': 'Hermosa pulsera con diseño elegante y moderno',
-                'price': 69.99,
-                'stock': 12,
-                'material': 'Metal y cristales',
+                'name': 'Topo Decorativo',
+                'description': 'Topo decorativo con diseño único. Perfecto para complementar cualquier atuendo.',
+                'price': 19.99,
+                'stock': 20,
+                'material': 'Metal, cristales',
                 'color': 'Plateado',
-                'dimensions': '19cm',
-                'images': [
-                    {'url': 'img/products/pulsoh1.jpg', 'is_main': True},
-                    {'url': 'img/products/pulsoh2.jpg', 'is_main': False},
-                    {'url': 'img/products/pulsoh3.jpg', 'is_main': False}
-                ]
-            },
-            {
-                'name': 'Broche Elegante',
-                'description': 'Hermoso broche con diseño elegante y moderno, perfecto para complementar cualquier atuendo. Fabricado con materiales de alta calidad y detalles delicados.',
-                'price': 59.99,
-                'stock': 8,
-                'material': 'Metal y cristales',
-                'color': 'Plateado y dorado',
-                'dimensions': '4cm x 3cm',
+                'dimensions': '3cm x 2cm',
                 'images': [
                     {'url': 'img/products/topo1.jpg', 'is_main': True},
                     {'url': 'img/products/topo2.jpg', 'is_main': False},
                     {'url': 'img/products/topo3.jpg', 'is_main': False},
                     {'url': 'img/products/topo4.jpg', 'is_main': False},
                     {'url': 'img/products/topo5.jpg', 'is_main': False}
+                ]
+            },
+            {
+                'name': 'Joyas de Plata',
+                'description': 'Conjunto de joyas de plata de alta calidad. Incluye collar y pulsera.',
+                'price': 39.99,
+                'stock': 12,
+                'material': 'Plata 925',
+                'color': 'Plateado',
+                'dimensions': 'Collar: 45cm, Pulsera: 18cm',
+                'images': [
+                    {'url': 'img/products/joyas1.png', 'is_main': True},
+                    {'url': 'img/products/joyas2.png', 'is_main': False},
+                    {'url': 'img/products/joyas3.png', 'is_main': False}
+                ]
+            },
+            {
+                'name': 'Conjunto de Plata',
+                'description': 'Elegante conjunto de joyas en plata con diseño moderno y minimalista.',
+                'price': 49.99,
+                'stock': 8,
+                'material': 'Plata 925',
+                'color': 'Plateado',
+                'dimensions': 'Collar: 40cm, Pulsera: 17cm',
+                'images': [
+                    {'url': 'img/products/plata1.jpg', 'is_main': True},
+                    {'url': 'img/products/plata2.jpg', 'is_main': False},
+                    {'url': 'img/products/plata3.jpg', 'is_main': False}
+                ]
+            },
+            {
+                'name': 'Conjunto de Corazones',
+                'description': 'Hermoso conjunto de joyas con motivos de corazones. Perfecto para regalar o usar en ocasiones especiales.',
+                'price': 34.99,
+                'stock': 15,
+                'material': 'Metal bañado en plata',
+                'color': 'Plateado',
+                'dimensions': 'Collar: 42cm, Pulsera: 18cm',
+                'images': [
+                    {'url': 'img/products/corazon1.jpg', 'is_main': True},
+                    {'url': 'img/products/corazon2.jpg', 'is_main': False},
+                    {'url': 'img/products/corazon3.jpg', 'is_main': False}
+                ]
+            },
+            {
+                'name': 'Set de Búho en Miyuki',
+                'description': 'Encantador conjunto de joyas con motivos de búho elaborado con cuentas Miyuki. Perfecto para amantes de la naturaleza y la moda.',
+                'price': 32.99,
+                'stock': 12,
+                'material': 'Cuentas Miyuki, hilo elástico',
+                'color': 'Multicolor',
+                'dimensions': 'Collar: 40cm, Pulsera: 18cm',
+                'images': [
+                    {'url': 'img/products/buho1.jpg', 'is_main': True},
+                    {'url': 'img/products/buho2.jpg', 'is_main': False}
+                ]
+            },
+            {
+                'name': 'Pulsera Tejida en Cristales',
+                'description': 'Pulsera tejida en cristales de miyuki con diseño elegante y moderno. Perfecta para complementar cualquier atuendo.',
+                'price': 27.99,
+                'stock': 15,
+                'material': 'Cristales Miyuki, hilo elástico',
+                'color': 'Multicolor',
+                'dimensions': 'Ajustable, aproximadamente 18cm',
+                'images': [
+                    {'url': 'img/products/pulsera1.jpg', 'is_main': True},
+                    {'url': 'img/products/pulsera2.jpg', 'is_main': False},
+                    {'url': 'img/products/pulsera3.jpg', 'is_main': False}
                 ]
             }
         ]
